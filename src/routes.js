ed4b83156398c25a996bfa48a520ac513072c27c
Ofir Oxenberg <ofir@playstudios-il.com>
2020-09-05 18:20:09 +0300

    [ofir][client] main, routes JS

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+import Main from "./pages/MainPage";
+import NotFound from "./pages/NotFoundPage";
+
+const routes = [{
+        path: "/",
+        name: "main",
+        component: Main,
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: () =>
+            import ("./pages/RegisterPage"),
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: () =>
+            import ("./pages/LoginPage"),
+    },
+    {
+        path: "/search",
+        name: "search",
+        // meta: {
+        //     requiresAuth: true,
+        // },
+        component: () =>
+            import ("./pages/SearchPage"),
+
+    },
+    {
+        path: "/recipe/:recipeId",
+        name: "recipe",
+        component: () =>
+            import ("./pages/RecipeViewPage"),
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: () =>
+            import ("./pages/AboutPage"),
+    },
+    {
+        path: "/myRecipes",
+        name: "myRecipes",
+        component: () =>
+            import ("./pages/MyRecipesPage"),
+    },
+    {
+        path: "/myFavorites",
+        name: "myFavorites",
+        component: () =>
+            import ("./pages/MyFavoritesPage"),
+    },
+    {
+        path: "*",
+        name: "notFound",
+        component: NotFound,
+    },
+];
+
+
+export default routes;
