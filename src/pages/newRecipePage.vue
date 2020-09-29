505350f1c98dacec1a5f76f250be54b599949d9b
Ofir Oxenberg <ofir@playstudios-il.com>
2020-09-29 17:55:03 +0300

    [client][ofir] fixes

--- src/pages/newRecipePage.vue
+++ src/pages/newRecipePage.vue
@@ -15,6 +15,12 @@
           type="text"
           :state="validateState('recipeName')">
           </b-form-input>
+          <b-form-invalid-feedback 
+          v-if="!$v.form.recipeName.required">
+          Recipe name is required</b-form-invalid-feedback>
+          <b-form-invalid-feedback
+          v-if="!$v.form.recipeName.alpha">
+          Recipe name must contain letters only</b-form-invalid-feedback>
         </b-form-group>
 
         <b-form-group
@@ -29,7 +35,7 @@
           type="text"
           :state="validateState('image')"
         ></b-form-input>
-        <b-form-invalid-feedback>Recipe picture is required</b-form-invalid-feedback>
+        <b-form-invalid-feedback v-if="!$v.form.image.required">Recipe picture is required</b-form-invalid-feedback>
         </b-form-group>
 
         <!-- <b-form-group id="input-group-recipePreparationTime" label-cols-sm="3" label="Country:" label-for="country">
@@ -41,6 +47,18 @@
         ></b-form-select>
         <b-form-invalid-feedback>Recipe preparation time is required</b-form-invalid-feedback>
         </b-form-group> -->
+
+        <b-row>
+          <b-col>
+            <b-form-select style="width:185px;" v-model="cuisine_chosen" :options="cuisines"></b-form-select>
+          </b-col>
+          <b-col>
+            <b-form-select style="width:185px;" v-model="diet_chosen" :options="diets"></b-form-select>
+          </b-col>
+          <b-col>
+            <b-form-select style="width:185px;" v-model="intolerance_chosen" :options="intolerances"></b-form-select>
+          </b-col>
+        </b-row>
 
         <b-form-group
         id="input-group-instruction"
@@ -56,11 +74,14 @@
           max-rows="20"
           :state="validateState('instruction')"
         ></b-form-textarea>
-        <b-form-invalid-feedback>Instruction is required</b-form-invalid-feedback>
+        <b-form-invalid-feedback 
+          v-if="!$v.form.instruction.required">
+          Instruction are required</b-form-invalid-feedback>
         </b-form-group> 
       
-        <b-form-checkbox class="mb-3">Vegetarian</b-form-checkbox>
-        <br />
+        <br/>
+        <br/>
+        <br/>
         <b-button type="submit" variant="danger" style="width:250px;">save recipe</b-button>
     </b-form>  
 </div>
@@ -68,38 +89,50 @@
 
 <script>
 import numbers from "../assets/numbers";
+import cuisines from "../assets/cuisine";
+import diets from "../assets/diet";
+import intolerances from "../assets/intolerance";
+
 import {
   required,
   alpha,
 } from "vuelidate/lib/validators";
 export default {
     data() {
-    return {
-      form: {
-        recipeName: "",
-        submitError: undefined
-      },
-      errors: [],
-      validated: false
-    }
+      return {
+        form: {
+          recipeName: "",
+          instruction: "",
+          submitError: undefined,
+          cuisine_chosen: null,
+            cuisines: [{ value: null, text: "-Select Cuisine-", disabled: true }],
+
+          diet_chosen: null,
+            diets: [{ value: null, text: "-Select Diet-", disabled: true }],
+
+          intolerance_chosen: null,
+            intolerances: [{ value: null, text: "-Select Intolerance-", disabled: true }],
+        },
+        errors: [],
+        validated: false
+      }
     },
     validations: {
-    form: {
-        recipeName: {
-            required,
-            alpha
-        },
-        image: {
-          required
-        },
-        // PreparationTime: {
-        //     required,
-        // },
-        instruction:{
-            required,
-            alpha
-        }
-     }
+      form: {
+          recipeName: {
+              required,
+              alpha
+          },
+          image: {
+            required
+          },
+          // PreparationTime: {
+          //     required,
+          // },
+          instruction:{
+              required
+          }
+      } 
     }, 
     methods: {
         validateState(param) {
@@ -114,7 +147,7 @@ export default {
 
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   max-width: 20000px;
   display: grid;
