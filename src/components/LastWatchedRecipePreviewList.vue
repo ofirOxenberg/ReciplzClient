1b43a8307e9be79942105ddbed9fc5809b962b0e
Ofir Oxenberg <ofir@playstudios-il.com>
2020-10-01 20:15:13 +0300

    [ofir] showMsg state change

--- src/components/LastWatchedRecipePreviewList.vue
+++ src/components/LastWatchedRecipePreviewList.vue
@@ -14,7 +14,7 @@ export default {
   data() {
     return {
       recipes: [],
-      showMsg: true,
+      showMsg: false,
     };
   },
   mounted() {
--- src/pages/MyRecipesPage.vue
+++ src/pages/MyRecipesPage.vue
@@ -18,7 +18,7 @@ export default {
   data() {
     return {
       recipes: [],
-      showMsg: true
+      showMsg: false
     };
   },
   mounted() {
