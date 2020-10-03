84b6fe57cbab0690109f07b8cc8bcfb1caeb69ad
Almog Oz <almogoz3@gmail.com>
2020-10-03 13:45:27 +0300

    bug

--- src/pages/MyMealsPage.vue
+++ src/pages/MyMealsPage.vue
@@ -26,13 +26,11 @@ export default {
   methods: {
     async updateRecipes() {
       try {
-        const meal_id = "0F70D914-3712-46E1-A3E5-48CEA53E6FE7"
+        const meal_id = "'0F70D914-3712-46E1-A3E5-48CEA53E6FE7'"
         const response = await this.axios.get(
           this.$root.store.BASE_URL +
            "/users/myMealRecipes" +
-            "[" +
-              meal_id +
-              "]"
+              meal_id 
         );
         var recipes_resopnse = response.data;
         this.recipes = [];
