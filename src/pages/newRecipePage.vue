7ed4d03e5588e865aa38de62e1cddc21c4f20b64
Ofir Oxenberg <ofir@playstudios-il.com>
2020-10-05 02:16:56 +0300

    fix

--- src/pages/newRecipePage.vue
+++ src/pages/newRecipePage.vue
@@ -153,17 +153,17 @@ export default {
         },
         minutes: [{ value: null, text: "", disabled: true }],
         serving: [
-              {value=null, text: 0, disabled: true},
-              {value="1" , text: 1},
-              {value="2" , text: 2},
-              {value="3" , text: 3},
-              {value="4" , text: 4},
-              {value="5" , text: 5},
-              {value="6" , text: 6},
-              {value="7" , text: 7},
-              {value="8" , text: 8},
-              {value="9" , text: 9},
-              {value="10" , text: 10}
+              {value: null, text: 0, disabled: true},
+              {value: "1" , text: 1},
+              {value: "2" , text: 2},
+              {value: "3" , text: 3},
+              {value: "4" , text: 4},
+              {value: "5" , text: 5},
+              {value: "6" , text: 6},
+              {value: "7" , text: 7},
+              {value: "8" , text: 8},
+              {value: "9" , text: 9},
+              {value: "10" , text: 10}
         ],
         errors: [],
         validated: false
