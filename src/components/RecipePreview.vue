12d983d35c78c1d47622b59d4f8005b4d3b5e2fd
Almog Oz <almogoz3@gmail.com>
2020-10-10 14:42:06 +0300

    test

--- src/components/RecipePreview.vue
+++ src/components/RecipePreview.vue
@@ -213,7 +213,7 @@ export default {
               "]"
           );
           this.watched = watch_and_save_response.data[this.recipe.id].watched;
-          this.saved = watch_and_save_response.data[this.recipe.id].saved;
+          this.saveTheRecipe = watch_and_save_response.data[this.recipe.id].saved;
         } catch (error) {
           console.log("error");
           return;
